76682da545fa641d1bc63ed3af3e9aa25b7d975a
Alois Napitalai <anapitalai@gmail.com>
2019-04-01 14:34:59 +1000

    Update teachers.js

--- api/routes/teachers.js
+++ api/routes/teachers.js
@@ -137,7 +137,7 @@ router.get('/',(req,res,next)=>{
 //get single alumni route
 router.get('/:memberId',(req,res,next)=>{
     const _id=req.params.memberId;
-    Teacher.find(_id)
+    Teacher.find({_id:_id})
     .select('_id name username nid avatarImage')
     .exec()
     .then(doc=>{
